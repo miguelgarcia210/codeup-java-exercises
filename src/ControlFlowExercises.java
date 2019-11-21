0d27c2337bedb96519a042e22f55b7b3a8a0818c
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-21 15:01:56 -0600

    Completed refactored while -> for-loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -23,10 +23,27 @@ public class ControlFlowExercises {
 //        } while (num >= -10);
 
         // TODO: Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-        int num = 2;
-        do {
-            System.out.print(num + "\n");
-            num = (int) Math.pow(num,2);
-        } while (num < 1000000);
+//        int num = 2;
+//        do {
+//            System.out.print(num + "\n");
+//            num = (int) Math.pow(num,2);
+//        } while (num < 1000000);
+
+        // TODO: Refactor previous exercises into for-loops
+//        for (byte i = 5; i <= 15; i++) {
+//            System.out.print(i + " ");
+//        }
+
+//        for (byte i = 0; i <= 100; i+=2) {
+//            System.out.println(i);
+//        }
+
+//        for (byte i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+
+        for (long i = 2; i < 1000000; i *= i) {
+            System.out.println(i);
+        }
     }
 }
