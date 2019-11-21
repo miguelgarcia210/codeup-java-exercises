2b593e6ecc0404a28746ae1fcb19ddde00a4ad46
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-21 11:53:31 -0600

    Exercise completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,19 +7,28 @@ public class ConsoleExercises {
 
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Enter an integer.");
-//        int userInput = scanner.nextInt();
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+        System.out.println("Enter an integer.");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
-//        System.out.println("Enter 3 words");
-//        String word1 = scanner.next();
-//        String word2 = scanner.next();
-//        String word3 = scanner.next();
-//        System.out.printf("%s\n%s\n%s\n", word1, word2, word3);
+        System.out.println("Enter 3 words");
+        String word1 = scanner.next();
+        String word2 = scanner.next();
+        String word3 = scanner.next();
+        System.out.printf("%s\n%s\n%s\n", word1, word2, word3);
 
-//        Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
         System.out.println("Enter a sentence.");
         String sentence = scanner.nextLine();
         System.out.println(sentence);
+
+        System.out.println("Enter the dimensions of a classroom at Codeup. (l,w,h)");
+        float length = scanner.nextFloat();
+        float width = scanner.nextFloat();
+        float height = scanner.nextFloat();
+
+        System.out.printf("Length: %.2f\nWidth: %.2f\nHeight: %.2f\n", length, width, height);
+        System.out.printf("Area: %.2f\n", (length * width));
+        System.out.printf("Perimeter: %.2f\n", ((length * 2) + (width * 2)));
+        System.out.printf("Volume: %.2f\n", (length * width * height));
     }
 }
