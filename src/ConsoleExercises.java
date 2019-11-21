1e7825ae51321fbdbd8b8db6cf755058283e58b8
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-21 10:52:59 -0600

    User Integer

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %s\n", pi);
+
+//        Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an integer.");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
     }
 }
