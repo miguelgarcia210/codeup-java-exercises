14db0e90c58a7257cba4e91c3c94a99cdd3e7e5b
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-21 11:04:03 -0600

    User sentence

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,14 +7,19 @@ public class ConsoleExercises {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter an integer.");
-        int userInput = scanner.nextInt();
-        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        System.out.println("Enter an integer.");
+//        int userInput = scanner.nextInt();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
-        System.out.println("Enter 3 words");
-        String word1 = scanner.next();
-        String word2 = scanner.next();
-        String word3 = scanner.next();
-        System.out.printf("%s\n%s\n%s\n", word1, word2, word3);
+//        System.out.println("Enter 3 words");
+//        String word1 = scanner.next();
+//        String word2 = scanner.next();
+//        String word3 = scanner.next();
+//        System.out.printf("%s\n%s\n%s\n", word1, word2, word3);
+
+//        Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
+        System.out.println("Enter a sentence.");
+        String sentence = scanner.nextLine();
+        System.out.println(sentence);
     }
 }
