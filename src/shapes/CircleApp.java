88cf7dd83352b17e5cc9eacc9e16fd43b6140a88
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-12-04 13:43:50 -0600

    Completed 1st part of bonus

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,13 @@
 package shapes;
 
+import util.Input;
+
 public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        Circle circle = new Circle(input.getDouble());
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Circumference: " + circle.getCircumference());
+    }
 }
